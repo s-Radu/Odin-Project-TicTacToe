83a94e06a4d3e7f3cf60cae7cae6ea1d1abfb2cf
lil.void <tsraduu@gmail.com>
2024-01-02 17:04:56 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# tic-tac-toe
+# Tic Tac Toe
+
+A tic tac toe game built for the learning trail of the [Odin Project](https://www.theodinproject.com/)
+
+---
+
+### Author
+
+A project built by [lil void](https://github.com/s-Radu)
+
+---
+
+### Technologies used:
+
+- Javascript
+- Tailwindcss
+
+---
+
+### Licence
+
+This project is not licenced and it's part of the Odin Project learning trail
+
+### Tree
+
+TO be added
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./**/*.{html,js}"],
+  purge: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      minHeight: {
+        card: "700px",
+      },
+      fontFamily: {
+        josefin: '"Josefin Sans", sans-serif',
+        montserat: '"Montserrat", sans-serif',
+        nunito: '"Nunito", sans-serif',
+      },
+      gridTemplateColumns: {
+        "auto-fill": "repeat(auto-fill, minmax(320px, 1fr))",
+      },
+      backgroundColor: ["before", "after"],
+      display: ["before", "after"],
+      content: ["before", "after"],
+    },
+  },
+  plugins: [],
+};
+
+//? npx tailwindcss -i ./assets/input.css -o ./assets/output.css --watch
