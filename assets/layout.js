11f88b19b72a1fd02aff6e850d63e1b7b60e1760
lil.void <tsraduu@gmail.com>
2024-01-02 18:51:37 +0000

    Function for layout built, just need to be created in the html first before adding it to layout.js

--- assets/layout.js
+++ assets/layout.js
@@ -1,5 +1,10 @@
 //! Display the layout on the page.
 
 (function () {
-  console.log("I'm layout.js");
+  const body = document.querySelector("body");
+  const layout = document.createElement("div");
+  layout.className = "mt-3 bg-gray-500 p-3 rounded-2xl";
+
+  layout.innerHTML = `<h1 class="font-josefin text-2xl underline text-center"> test </h1>`;
+  body.appendChild(layout);
 })();
