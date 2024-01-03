066150d15894df876f7449a793b6694e3355fcd1
lil.void <tsraduu@gmail.com>
2024-01-03 20:08:48 +0000

    Nav bar
    Added nav bar with JS after styling it in the html

--- assets/layout.js
+++ assets/layout.js
@@ -2,9 +2,28 @@
 
 (function () {
   const body = document.querySelector("body");
+  const navBar = document.createElement("nav");
   const layout = document.createElement("div");
-  layout.className = "mt-3 bg-gray-500 p-3 rounded-2xl";
+
+  layout.className = "container mt-3 bg-gray-300 p-3 rounded-2xl mx-auto";
+  navBar.className =
+    "container bg-gray-300 rounded-2xl mx-auto flex justify-between";
+
+  navBar.innerHTML = `<nav class="container bg-gray-300 rounded-2xl mx-auto flex justify-between items-center">
+    <div class="container flex justify-between">
+        <img src="img/navImg.svg" alt="Logo" class="h-14 m-2 cursor-pointer">
+        <div class="items-center flex">
+            <button class="rounded-xl font-josefin bg-blue-400 m-3 hover:bg-blue-500 p-1 ">
+                Start game
+            </button>
+            <button class="rounded-xl font-josefin bg-blue-400 m-3 hover:bg-blue-500 p-1 ">
+                Reset game
+            </button>
+        </div>
+    </div>
+</nav>`;
 
   layout.innerHTML = `<h1 class="font-josefin text-2xl underline text-center"> test </h1>`;
-  body.appendChild(layout);
+  body.appendChild(navBar);
+  //   body.appendChild(layout);
 })();
