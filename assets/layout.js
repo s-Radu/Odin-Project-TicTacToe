af867a8f3c7d59e301735b2bb04efc88c8dc844a
lil.void <tsraduu@gmail.com>
2024-01-04 20:55:41 +0000

    moved HTML boilerplate into JS and tested to see if it works

--- assets/layout.js
+++ assets/layout.js
@@ -23,7 +23,51 @@
     </div>
 </nav>`;
 
-  layout.innerHTML = `<h1 class="font-josefin text-2xl underline text-center"> test </h1>`;
+  layout.innerHTML = `<div class="container bg-gray-300 rounded-2xl mx-auto flex flex-col justify-between m-3">
+  <div class="flex flex-col justify-between mx-auto m-3 items-center">
+      <h1 class="font-josefin text-2xl">Player?</h1>
+
+      <div class="">
+          <button
+              class="bg-transparent w-20 border-2 border-black rounded-xl m-3 hover:bg-gray-200 ease-in duration-300 shadow-btn">X</button>
+          <button
+              class="bg-transparent w-20 border-2 border-black rounded-xl m-3 hover:bg-gray-200 ease-in duration-300 shadow-btn">O</button>
+      </div>
+
+  </div>
+
+  <div class="container mx-auto w-3/4 p-3 h-96">
+      <div class="grid grid-cols-3 gap-4 h-full place-items-center">
+          <div
+              class="gameCard w-2/4 h-full border-2 border-black rounded-xl hover:cursor-pointer relative  after:absolute after:left-2/4 after:top-2/4 after:text-9xl after:-translate-x-1/2 after:-translate-y-1/2">
+          </div>
+          <div
+              class="gameCard w-2/4 h-full border-2 border-black rounded-xl hover:cursor-pointer relative  after:absolute after:left-2/4 after:top-2/4 after:text-9xl after:-translate-x-1/2 after:-translate-y-1/2">
+          </div>
+          <div
+              class="gameCard w-2/4 h-full border-2 border-black rounded-xl hover:cursor-pointer relative  after:absolute after:left-2/4 after:top-2/4 after:text-9xl after:-translate-x-1/2 after:-translate-y-1/2">
+          </div>
+          <div
+              class="gameCard w-2/4 h-full border-2 border-black rounded-xl hover:cursor-pointer relative  after:absolute after:left-2/4 after:top-2/4 after:text-9xl after:-translate-x-1/2 after:-translate-y-1/2">
+          </div>
+          <div
+              class="gameCard w-2/4 h-full border-2 border-black rounded-xl hover:cursor-pointer relative  after:absolute after:left-2/4 after:top-2/4 after:text-9xl after:-translate-x-1/2 after:-translate-y-1/2">
+          </div>
+          <div
+              class="gameCard w-2/4 h-full border-2 border-black rounded-xl hover:cursor-pointer relative  after:absolute after:left-2/4 after:top-2/4 after:text-9xl after:-translate-x-1/2 after:-translate-y-1/2">
+          </div>
+          <div
+              class="gameCard w-2/4 h-full border-2 border-black rounded-xl hover:cursor-pointer relative  after:absolute after:left-2/4 after:top-2/4 after:text-9xl after:-translate-x-1/2 after:-translate-y-1/2">
+          </div>
+          <div
+              class="gameCard w-2/4 h-full border-2 border-black rounded-xl hover:cursor-pointer relative  after:absolute after:left-2/4 after:top-2/4 after:text-9xl after:-translate-x-1/2 after:-translate-y-1/2">
+          </div>
+          <div
+              class="gameCard w-2/4 h-full border-2 border-black rounded-xl hover:cursor-pointer relative  after:absolute after:left-2/4 after:top-2/4 after:text-9xl after:-translate-x-1/2 after:-translate-y-1/2">
+          </div>
+      </div>
+  </div>
+</div>`;
   body.appendChild(navBar);
-  //   body.appendChild(layout);
+  body.appendChild(layout);
 })();
