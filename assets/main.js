8020d0d2fa2257d345bc2ca6843485987ed9e380
lil.void <tsraduu@gmail.com>
2024-01-04 20:54:04 +0000

    Game somewhat finished being built on HTML and needs transfering to JS
    Also added a function to test if I can add X and O with tailwindcss After content

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,18 @@
 import "./layout.js";
+
+const test = document.querySelectorAll(".gameCard");
+
+function toggleXO(e) {
+  const card = e.target;
+  if (card.classList.contains("after:content-['X']")) {
+    card.classList.remove("after:content-['X']");
+    card.classList.add("after:content-['O']");
+  } else {
+    card.classList.remove("after:content-['O']");
+    card.classList.add("after:content-['X']");
+  }
+}
+
+test.forEach((card) => {
+  card.addEventListener("click", toggleXO);
+});
