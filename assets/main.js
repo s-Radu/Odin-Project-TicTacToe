4db5021065067d9bb07c41d1c26f6dd96f5de8ac
lil.void <tsraduu@gmail.com>
2024-01-06 17:50:08 +0000

    Added player marker
    After a lot of back and forth, i think it's easyer to just assign the second player the marker directly

--- assets/main.js
+++ assets/main.js
@@ -1,32 +1,51 @@
 import "./layout.js";
 
-const test = document.querySelectorAll(".gameCard");
-const players = document.querySelectorAll(".player");
+const game = (function () {
+  //* create player
 
-function toggleXO(e) {
-  const card = e.target;
-  //   if (!card.hasChildNodes()) { //* Both of these conditions work, initially tried with reverse condition on hasChildNodes, but encountered an error where I had empty spaces in HTML and it would not work, because hasChildNodes checks for empty spaces as well ( white space ).
-  if (card.textContent.trim() === "") {
-    const element = document.createElement("p");
-    element.innerText = "X";
-    card.appendChild(element);
-  } else {
-    console.log("You can't park there mate");
+  function _createPlayer(name, marker) {
+    return {
+      name,
+      marker,
+    };
   }
-}
 
-function checkPlayer(e) {
-  if (e.target.textContent === "X") {
-    console.log("Player X");
-  } else {
-    console.log("Player O");
+  //* store player
+
+  let PLAYER1 = "";
+  let PLAYER2 = "";
+  // let playerAsigned = false;
+
+  function _storePlayer(e) {
+    const chosenMarker = e.target.textContent;
+    PLAYER1 = _createPlayer("Player 1", chosenMarker);
+    PLAYER1.marker === "X"
+      ? (PLAYER2 = _createPlayer("Player 2", "O"))
+      : (PLAYER2 = _createPlayer("Player 2", "X"));
   }
-}
 
-test.forEach((card) => {
-  card.addEventListener("click", toggleXO);
-});
+  //* cache DOM
+  const test = document.querySelectorAll(".gameCard");
+  const players = document.querySelectorAll(".player");
 
-players.forEach((player) => {
-  player.addEventListener("click", checkPlayer);
-});
+  // * bind events
+  test.forEach((card) => {
+    card.addEventListener("click", _toggleXO);
+  });
+
+  players.forEach((player) => {
+    player.addEventListener("click", _storePlayer);
+  });
+
+  function _toggleXO(e) {
+    const card = e.target;
+    //   if (!card.hasChildNodes()) { //* Both of these conditions work, initially tried with reverse condition on hasChildNodes, but encountered an error where I had empty spaces in HTML and it would not work, because hasChildNodes checks for empty spaces as well ( white space ).
+    if (card.textContent.trim() === "") {
+      const element = document.createElement("p");
+      element.innerText = "X";
+      card.appendChild(element);
+    } else {
+      console.log("You can't park there mate");
+    }
+  }
+})();
